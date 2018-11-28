e6b1d00b0ebb0808eb55dbc6d958af593e4ab2c5
kian ceelie <rra.amec1x.ionic.blok1@gmail.com>
2018-11-28 09:37:20 +0100

    InfiniteRunner
    Game voor project 2

---
+++ SuperZombieRunner/Assets/Scripts/PixelPerfectCamera.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PixelPerfectCamera : MonoBehaviour {
+
+    public static float PixelToUnits = 1f;
+    public static float scale = 1f;
+
+    public Vector2 nativeresolution = new Vector2(240, 160);
+
+	// Use this for initialization
+	void Awake () {
+        var camera = GetComponent<Camera>();
+
+        if (camera.orthographic)
+        {
+            scale = Screen.height / nativeresolution.y;
+            PixelToUnits *= scale;
+            camera.orthographicSize = (Screen.height / 2.0f) / PixelToUnits;
+        }
+	}
+
+}
+
+
---
+++ SuperZombieRunner/Assets/Scripts/TiledBackground.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TiledBackground : MonoBehaviour {
+
+    public int TextureSize = 32;
+    public bool ScaleHorizontally = true;
+    public bool ScaleVertically = true;
+
+	// Use this for initialization
+	void Start () {
+
+        var newWidth = !ScaleHorizontally ? 1 : Mathf.Ceil(Screen.width / (TextureSize * PixelPerfectCamera.scale));
+        var newHeight = !ScaleVertically ? 1 : Mathf.Ceil(Screen.height / (TextureSize * PixelPerfectCamera.scale));
+
+        transform.localScale = new Vector3(newWidth * TextureSize, newHeight * TextureSize, 1);
+
+        GetComponent<Renderer>().material.mainTextureScale = new Vector3(newWidth, newHeight, 1);
+
+    }
+	
+}
